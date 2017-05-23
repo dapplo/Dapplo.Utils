c74f3e6929d68f44369c0b338570e4b3ab4a40d9
Robin <robin@dapplo.net>
2017-05-23 09:17:35 +0200

    Trying to fix the build. [release]

--- Dapplo.Utils.Tests/TypeExtensionsTests.cs
+++ Dapplo.Utils.Tests/TypeExtensionsTests.cs
@@ -34,101 +34,111 @@ using Dapplo.Log.XUnit;
 
 namespace Dapplo.Utils.Tests
 {
-	public class TypeExtensionsTests
-	{
-		public TypeExtensionsTests(ITestOutputHelper testOutputHelper)
-		{
-			LogSettings.RegisterDefaultLogger<XUnitLogger>(LogLevels.Verbose, testOutputHelper);
-		}
+    public class TypeExtensionsTests
+    {
+        public TypeExtensionsTests(ITestOutputHelper testOutputHelper)
+        {
+            LogSettings.RegisterDefaultLogger<XUnitLogger>(LogLevels.Verbose, testOutputHelper);
+        }
 
-		[Fact]
-		public void TestAdd()
-		{
-			// Added to get better coverage
-			TypeExtensions.AddDefaultConverter(typeof(TypeExtensionsTests), null);
-		}
+        [Fact]
+        public void TestAdd()
+        {
+            // Added to get better coverage
+            TypeExtensions.AddDefaultConverter(typeof(TypeExtensionsTests), null);
+        }
 
-		[Fact]
-		public void TestConvertOrCastValueToType_NullAndEmpty()
-		{
-			var result = typeof(string).ConvertOrCastValueToType(null);
-			Assert.Null(result);
-			// Added to get better coverage
-			result = TypeExtensions.ConvertOrCastValueToType<string>(null);
-			Assert.Null(result);
+        [Fact]
+        public void TestConvertOrCastValueToType_StringArrayToUriArray()
+        {
+            var result = typeof(Uri[]).ConvertOrCastValueToType(new [] { "http://1.dapplo.net" , "http://2.dapplo.net" }) as Uri[];
+            Assert.NotNull(result);
+            Assert.Contains(new Uri("http://1.dapplo.net"), result);
+            Assert.Equal(2, result.Length);
+        }
 
-			var stringResult = typeof(string).ConvertOrCastValueToType("", new StringConverter()) as string;
-			Assert.NotNull(stringResult);
-			Assert.True(stringResult.Length == 0);
 
-			stringResult = typeof(string).ConvertOrCastValueToType("") as string;
-			Assert.NotNull(stringResult);
-			Assert.True(stringResult.Length == 0);
-		}
+        [Fact]
+        public void TestConvertOrCastValueToType_NullAndEmpty()
+        {
+            var result = typeof(string).ConvertOrCastValueToType(null);
+            Assert.Null(result);
+            // Added to get better coverage
+            result = TypeExtensions.ConvertOrCastValueToType<string>(null);
+            Assert.Null(result);
 
-		[Fact]
-		public void TestConvertOrCastValueToType_StringToListOfString()
-		{
-			var listOfStringType = typeof(List<string>);
+            var stringResult = typeof(string).ConvertOrCastValueToType("", new StringConverter()) as string;
+            Assert.NotNull(stringResult);
+            Assert.True(stringResult.Length == 0);
 
-			var listOfStringsObject = listOfStringType.ConvertOrCastValueToType("1,2,3");
-			Assert.NotNull(listOfStringsObject);
-			var listOfStrings = listOfStringsObject as IList<string>;
-			Assert.NotNull(listOfStrings);
+            stringResult = typeof(string).ConvertOrCastValueToType("") as string;
+            Assert.NotNull(stringResult);
+            Assert.True(stringResult.Length == 0);
+        }
 
-			Assert.NotNull(listOfStrings.Count == 3);
-		}
+        [Fact]
+        public void TestConvertOrCastValueToType_StringToListOfString()
+        {
+            var listOfStringType = typeof(List<string>);
 
-		[Fact]
-		public void TestConvertOrCastValueToType_StringToListOfInt()
-		{
-			var listOfStringType = typeof(List<int>);
+            var listOfStringsObject = listOfStringType.ConvertOrCastValueToType("1,2,3");
+            Assert.NotNull(listOfStringsObject);
+            var listOfStrings = listOfStringsObject as IList<string>;
+            Assert.NotNull(listOfStrings);
 
-			var listOfIntssObject = listOfStringType.ConvertOrCastValueToType("1,2,3");
-			Assert.NotNull(listOfIntssObject);
-			var listOfInts = listOfIntssObject as IList<int>;
-			Assert.NotNull(listOfInts);
+            Assert.NotNull(listOfStrings.Count == 3);
+        }
 
-			Assert.NotNull(listOfInts.Count == 3);
-		}
+        [Fact]
+        public void TestConvertOrCastValueToType_StringToListOfInt()
+        {
+            var listOfStringType = typeof(List<int>);
 
-		[Fact]
-		public void TestConvertOrCastValueToType_StringToIListOfInt()
-		{
-			var listOfStringType = typeof(IList<int>);
+            var listOfIntssObject = listOfStringType.ConvertOrCastValueToType("1,2,3");
+            Assert.NotNull(listOfIntssObject);
+            var listOfInts = listOfIntssObject as IList<int>;
+            Assert.NotNull(listOfInts);
 
-			var listOfIntssObject = listOfStringType.ConvertOrCastValueToType("1,2,3");
-			Assert.NotNull(listOfIntssObject);
-			var listOfInts = listOfIntssObject as IList<int>;
-			Assert.NotNull(listOfInts);
+            Assert.NotNull(listOfInts.Count == 3);
+        }
 
-			Assert.NotNull(listOfInts.Count == 3);
-		}
+        [Fact]
+        public void TestConvertOrCastValueToType_StringToIListOfInt()
+        {
+            var listOfStringType = typeof(IList<int>);
 
-		[Fact]
-		public void TestDefault()
-		{
-			var defaultBool = typeof(bool).Default();
-			Assert.Equal(false, defaultBool);
-			var defaultInt = typeof(int).Default();
-			Assert.Equal(0, defaultInt);
-			var defaultString = typeof(string).Default();
-			Assert.Equal(null, defaultString);
-			var defaultListOfString = typeof(List<string>).Default();
-			Assert.Equal(null, defaultListOfString);
-		}
+            var listOfIntssObject = listOfStringType.ConvertOrCastValueToType("1,2,3");
+            Assert.NotNull(listOfIntssObject);
+            var listOfInts = listOfIntssObject as IList<int>;
+            Assert.NotNull(listOfInts);
 
-		[Fact]
-		public void TestFriendlyName()
-		{
-			Assert.Equal("string", typeof(string).FriendlyName());
-			Assert.Equal("int[]", typeof(int[]).FriendlyName());
-			Assert.Equal("int[][]", typeof(int[][]).FriendlyName());
-			Assert.Equal("KeyValuePair<int, string>", typeof(KeyValuePair<int, string>).FriendlyName());
-			Assert.Equal("Tuple<int, string>", typeof(Tuple<int, string>).FriendlyName());
-			Assert.Equal("Tuple<KeyValuePair<object, long>, string>", typeof(Tuple<KeyValuePair<object, long>, string>).FriendlyName());
-			Assert.Equal("List<Tuple<int, string>>", typeof(List<Tuple<int, string>>).FriendlyName());
-			Assert.Equal("Tuple<short[], string>", typeof(Tuple<short[], string>).FriendlyName());
-		}
-	}
+            Assert.NotNull(listOfInts.Count == 3);
+        }
+
+        [Fact]
+        public void TestDefault()
+        {
+            var defaultBool = typeof(bool).Default();
+            Assert.Equal(false, defaultBool);
+            var defaultInt = typeof(int).Default();
+            Assert.Equal(0, defaultInt);
+            var defaultString = typeof(string).Default();
+            Assert.Equal(null, defaultString);
+            var defaultListOfString = typeof(List<string>).Default();
+            Assert.Equal(null, defaultListOfString);
+        }
+
+        [Fact]
+        public void TestFriendlyName()
+        {
+            Assert.Equal("string", typeof(string).FriendlyName());
+            Assert.Equal("int[]", typeof(int[]).FriendlyName());
+            Assert.Equal("int[][]", typeof(int[][]).FriendlyName());
+            Assert.Equal("KeyValuePair<int, string>", typeof(KeyValuePair<int, string>).FriendlyName());
+            Assert.Equal("Tuple<int, string>", typeof(Tuple<int, string>).FriendlyName());
+            Assert.Equal("Tuple<KeyValuePair<object, long>, string>", typeof(Tuple<KeyValuePair<object, long>, string>).FriendlyName());
+            Assert.Equal("List<Tuple<int, string>>", typeof(List<Tuple<int, string>>).FriendlyName());
+            Assert.Equal("Tuple<short[], string>", typeof(Tuple<short[], string>).FriendlyName());
+        }
+    }
 }
